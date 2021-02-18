68d29cd6794a1e0376995e5a1fb1d432cfe244f8
Sai Tushaar K M <20cse0201@vvce.ac.in>
2021-02-18 17:26:59 +0000

    maximum and minimum values in an array

--- Program_5.c
+++ Program_5.c
@@ -5,13 +5,24 @@
 
 int main()
 {
-    int arr[10], n, i;
+    int arr[10], n, i,max,min;
+    printf("Enter the number of elements in the array\n");
     scanf("%d", &n);
+    printf("Enter the elements of the array\n");
 
     for(i = 0; i < n; i++)
         scanf("%d", &arr[i]);
-
-    //Write your code here
-  
+        max = arr[0];
+        min = arr[0];
+        for(i=1;i<n;i++)
+        {
+          if(arr[i]>max)
+          max = arr[i];
+          if(arr[i]<min)
+          min = arr[i];
+        }
+        printf("max = %d\nmin = %d",max,min);
+    
+    //Write your code here 
     return 0;
 }
