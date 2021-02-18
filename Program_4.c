18a27add281381bab16b88e381a99ff9efbc7cd5
Sai Tushaar K M <20cse0201@vvce.ac.in>
2021-02-18 12:26:25 +0000

    sum of the array

--- Program_4.c
+++ Program_4.c
@@ -7,8 +7,18 @@
 #include <stdio.h>
 int main()
 {
+  int N,a[5],i,sum=0;
   //Declare all the variables[N, array a(1D array), i, sum=0] used in the code
+  printf("Enter the array size:\n");
+  scanf("%d",&N);
   //Read the Value of array size N using scanf statement
+  printf("Enter the array elements\n");
+  for(i=0;i<N;i++)
+  {
+    scanf("%d",&a[i]);
+    sum = sum + a[i];    
+  }
+  printf("The sum of the array elements is:%d\n",sum);
   //Read the array elements using for loop and scanf statment
   // DO the addition of array elements and store the final result in variable sum
   //Print the sum of array elements which is stored in variable sum using printf statement
